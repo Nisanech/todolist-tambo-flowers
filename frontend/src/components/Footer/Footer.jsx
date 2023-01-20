a16d4d9ee781537a73fa859f74bbe1db655c8408
Karen Andrea Niño Angulo <daka201828@gmail.com>
2023-01-20 10:22:13 -0500

    Header y Footer

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Footer.css";
+
+
+const Footer = () => {
+    return(
+        <footer>
+        <ul>
+            <li>
+                <img src="https://drive.google.com/uc?export=view&id=1yGa764QyRviYkilTma4_uud1N2-WsBli" alt="Logo Edward" />
+            </li>
+            <li>
+                <img src="https://drive.google.com/uc?export=view&id=13hek3IczUeQC96eeZlzF8JfnPkSbZlcp" alt="Logo Cristian" />
+            </li>
+            <li>
+                <img src="https://drive.google.com/uc?export=view&id=1KvQGPpf-vbfZG_04THsY0uSYCEjIolCa" alt="Logo Juli" />
+            </li>
+            <li>
+                <img src="https://drive.google.com/uc?export=view&id=1FrSmkrVzuS-GNHHEIgZWNUBQFYBx4R_g" alt="Logo Nico" />
+            </li>
+            <li>
+                <img src="https://drive.google.com/uc?export=view&id=1kM4l26zgEHV4xEuG9-IkZA-gs2lj_cDK" alt="Logo Andre" />
+            </li>
+            <li>
+                <img src="https://drive.google.com/uc?export=view&id=1zMNuLxkZCQM9yIbDDh8r-dLelrANndK9" alt="Logo Vanegas" />
+            </li>
+        </ul>
+        </footer>
+)
+}
+
+export default Footer;
