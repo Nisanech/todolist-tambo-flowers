8ab33c35a4dd33d542a63c81df86464adf827941
Jonathan Vanegas <jonathanvanegasg@hotmail.com>
2023-01-23 15:01:39 -0500

    subbiendo

--- frontend/src/compone/TodoList.jsx
+++ frontend/src/compone/TodoList.jsx
@@ -1,70 +1,79 @@
-import React, { useState } from 'react';
-import TodoForm from './TodoForm';
-import Todo from './Todo';
-import { BsSearch} from 'react-icons/bs';
-import "../App.css"
+import React, { useState } from "react";
+import TodoForm from "./TodoForm";
+import Todo from "./Todo";
+import { BsSearch } from "react-icons/bs";
+import "../App.css";
+import Swal from 'react-sweetalert2';
+
+
 
 function TodoList() {
-    const [todos, setTodos] = useState([]);
+ const showAlert=()=>{
+  alert('si')
+ }
 
-    const addTodo = todo => {
-        if (!todo.text || /^\s*$/.test(todo.text)) {
-            return;
-        }
+  const [todos, setTodos] = useState([]);
 
-        const newTodos = [todo, ...todos];
+  const addTodo = (todo) => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
 
-        setTodos(newTodos);
-        console.log(...todos);
-    };
+    const newTodos = [todo, ...todos];
 
-    const updateTodo = (todoId, newValue) => {
-        if (!newValue.text || /^\s*$/.test(newValue.text)) {
-            return;
-        }
+    setTodos(newTodos);
+    console.log(...todos);
+  };
 
-        setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)));
-    };
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
+    }
 
-    const removeTodo = id => {
-        const removedArr = [...todos].filter(todo => todo.id !== id);
-
-        setTodos(removedArr);
-    };
-
-    const completeTodo = id => {
-        let updatedTodos = todos.map(todo => {
-            if (todo.id === id) {
-                todo.isComplete = !todo.isComplete;
-            }
-            return todo;
-        });
-        setTodos(updatedTodos);
-    };
-
-    return (
-        <>
-            <h1>AÑADIR TAREA</h1>
-            <TodoForm onSubmit={addTodo} />
-            <h1> Lista de tareas</h1>
-            <div> 
-                <input className='search' type="text" placeholder='Buscar tarea'/>
-                <BsSearch 
-                    /* onClick={() => removeTodo(todo.id)}
-                    className='delete-icon' */
-                />
-                <h2> VER HISTORIAL DE TAREAS </h2>
-                
-            </div>
-            <Todo
-                todos={todos}
-                completeTodo={completeTodo}
-                removeTodo={removeTodo}
-                updateTodo={updateTodo}
-            />
-
-        </>
+    setTodos((prev) =>
+      prev.map((item) => (item.id === todoId ? newValue : item))
     );
+  };
+
+  const removeTodo = (id) => {
+    const removedArr = [...todos].filter((todo) => todo.id !== id);
+
+    setTodos(removedArr);
+  };
+
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <>
+      <h1>AÑADIR TAREA</h1>
+      <TodoForm onSubmit={addTodo} />
+      <h1> Lista de tareas</h1>
+      <div>
+        <input className="search" type="text" placeholder="Buscar tarea" />
+        <BsSearch
+        /* onClick={() => removeTodo(todo.id)}
+                    className='delete-icon' */
+        />
+         <button onClick={{showAlert}}>
+            Open
+        </button>
+      </div>
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
+    </>
+  );
 }
 
 export default TodoList;
