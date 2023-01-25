c88c81da9bee91bf22392902193b8d58b6cfceeb
Nisanech <57728976+Nisanech@users.noreply.github.com>
2023-01-25 17:35:12 -0500

    update

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,18 @@ const app = express();
 app.use(express.json());
 
 // Use Cors
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 
 // Función para hacer uso de la conexión de la base de datos
 connectDB();
