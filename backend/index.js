1579ae685d0468f3ff0d04345e3ec40823d4d9bf
Nisanech <57728976+Nisanech@users.noreply.github.com>
2023-01-25 17:45:24 -0500

    update

--- backend/index.js
+++ backend/index.js
@@ -22,17 +22,6 @@ app.use(express.json());
 // Use Cors
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
-
 // Función para hacer uso de la conexión de la base de datos
 connectDB();
 
--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -12,7 +12,17 @@ const {
 } = require("../controllers/todoController");
 
 // Routes for the API
-router.get("/todos", getAllTodos); // Get task
+router.get("/todos", getAllTodos, (res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+}); // Get task
 router.post("/todo/new", addTodo); // Create task
 router.put("/todo/editTodo/:id", updateTodo); // Edit task
 router.get("/todo/toggleStatus/:id", toggleTodoDone); // Completed status
